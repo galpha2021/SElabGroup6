aacdc03f065b00b19df2ae7a4c0f519d01c846fb
Kev1225 <156151111+Kev1225@users.noreply.github.com>
2025-04-14 20:02:27 -0500

    Made changes to seller can moderate product
    -Made changes to views.py: Added a new view function seller_dashboard() that requires the user to be logged in, queries the database for all items listed by the logged in seller.
    -Changes to homepage.html: added a nav link so that if a seller is logged in, they see a seller dashboard link in the navbar
    -Changes to urls.py: Addded a new route dashboard under the store app. provides a dedicated url for sellers to view and manage their listed products.
    -Changes to main urls.py: Included the store urls under the seller route.
    -Changes to models.py: Created a new Item model to represent products listed by sellers.
    -New seller_dashboard.html: Created a new HTML that displays the seller's products. It also handles the case where the seller has no products listed.

--- urls.py
+++ urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .views import DeleteAccountView
 from . import views
+from .views import seller_dashboard
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('delete/', DeleteAccountView.as_view(), name='delete_account'),
-    path('homepage/', views.homepage_view, name='home'),
+    path('', views.homepage_view, name='home'),
     path('logout/', views.logout_view, name='logout'),
     path('loggedout/', views.loggedout_view, name='loggedout'),
-    path('seller/', views.seller_view, name='seller'),
+
+    
+    path('seller/dashboard/', seller_dashboard, name='seller_dashboard'),
 ]

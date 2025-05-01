4c238de7ba4c85249b6ee1015fd02f622ce8a6c0
Siriusglow <116605877+Siriusglow@users.noreply.github.com>
2025-04-30 21:11:35 -0500

    Bug Fixes to Admin Page #42
    This includes some bug fixes for the admin page.

--- users/urls.py
+++ users/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     #path('seller/delete/', views.delete_item, name='seller_delete'),
     path('seller/update-or-delete/', views.seller_update_or_delete, name='seller_update_or_delete'),
     path('cart/', views.view_cart, name='view_cart'),
-]
+    path('admin/monitor/users/', views.admin_monitor_users, name='admin_monitor_users'),
+    path('admin/monitor/products/', views.admin_monitor_products, name='admin_monitor_products'),
+    path('admin/delete_account/<int:user_id>/', views.admin_delete_account, name='admin_delete_account'),]
